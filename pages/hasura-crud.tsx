2a6e093af017d6a7ea096daa67b45532d7e93b55
aoba <ipbdipbd00@gmail.com>
2021-05-03 06:18:50 +0900

    create crud page

---
+++ pages/hasura-crud.tsx
@@ -0,0 +1,116 @@
+import { VFC, useState, FormEvent } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import {
+  GET_USERS,
+  CREATE_USER,
+  DELETE_USER,
+  UPDATE_USER,
+} from '../queries'
+import {
+  GetUsersQuery,
+  CreateUserMutation,
+  DeleteUserMutation,
+  UpdateUserMutation,
+} from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+import { UserItem } from '../components/UserItem'
+
+const HasuraCrudPage: VFC = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'cache-and-network',
+  })
+  const [updateUsersByPk] = useMutation<UpdateUserMutation>(UPDATE_USER)
+  const [insertUsersOne] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUsers, { toReference }) {
+            return [toReference(cacheId), ...existingUsers]
+          },
+        },
+      })
+    },
+  })
+  const [deleteUsersByPk] = useMutation<DeleteUserMutation>(DELETE_USER, {
+    update(cache, { data: { delete_users_by_pk } }) {
+      cache.modify({
+        fields: {
+          users(existingUsers, { readField }) {
+            return existingUsers.filter(
+              (user) => delete_users_by_pk.id !== readField('id', user)
+            )
+          },
+        },
+      })
+    },
+  })
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedUser.id) {
+      try {
+        await updateUsersByPk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    } else {
+      try {
+        await insertUsersOne({
+          variables: {
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    }
+  }
+  if (error) return <Layout title="Hasura CRUD">Error: {error.message}</Layout>
+  return (
+    <Layout title="Hasura CRUD">
+      <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="px-3 py-2 border border-gray-300"
+          placeholder="New user ?"
+          type="text"
+          value={editedUser.name}
+          onChange={(e) =>
+            setEditedUser({ ...editedUser, name: e.target.value })
+          }
+        />
+        <button
+          disabled={!editedUser.name}
+          className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
+
+      {data?.users.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            user={user}
+            setEditedUser={setEditedUser}
+            deleteUsersByPk={deleteUsersByPk}
+          />
+        )
+      })}
+    </Layout>
+  )
+}
+export default HasuraCrudPage
